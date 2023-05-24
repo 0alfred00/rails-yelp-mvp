fa61eb4d6a30e4083a16a0548e78a8c6feb6d65e
Alfred Seeliger <seeliger.alfred@gmail.com>
2023-05-24 12:50:05 +0200

    set up routes, controllers, models, dbs and seeded data, prepared the views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
 end
+
+# chatgpt version below
+
+# Rails.application.routes.draw do
+#   resources :restaurants, only: [:index, :new, :create, :show] do
+#     resources :reviews, only: [:new, :create]
+#   end
+# end
